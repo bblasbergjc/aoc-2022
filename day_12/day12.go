0619c97fdb99d23c1c0602097e3132e5ed27b765
Ben Blasberg <ben.blasberg@jumpcloud.com>
2022-12-12 10:34:30 -0600

    Completed part one

---
+++ day_12/day12.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+const debug = false
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ifDebug(action func()) {
+	if debug {
+		action()
+	}
+}
+
+type Point struct {
+	Row int
+	Col int
+	Val rune
+}
+
+// builds the grid and returns the starting and ending points
+func buildGrid(lines []string) ([][]rune, Point, Point) {
+	var start Point
+	var end Point
+	grid := make([][]rune, len(lines))
+	for row, line := range lines {
+		heights := make([]rune, len(line))
+		for col, ch := range line {
+			heights[col] = ch
+
+			if ch == 'S' {
+				start = Point{row, col, ch}
+			} else if ch == 'E' {
+				end = Point{row, col, ch}
+			}
+		}
+		grid[row] = heights
+	}
+
+	return grid, start, end
+}
+
+func getPoint(grid [][]rune, row int, col int) *Point {
+	if row >= len(grid) || row < 0 {
+		return nil
+	}
+
+	if col >= len(grid[0]) || col < 0 {
+		return nil
+	}
+	return &Point{row, col, grid[row][col]}
+}
+
+func isValidMoveFunc(grid [][]rune, start Point, visited [][]bool) func(int, int) (Point, bool) {
+	return func(row, col int) (Point, bool) {
+		next := getPoint(grid, row, col)
+
+		if next == nil {
+			return Point{}, false
+		}
+
+		nextVal := next.Val
+		if nextVal == 'E' {
+			nextVal = 'z'
+		}
+
+		startVal := start.Val
+		if startVal == 'S' {
+			startVal = 'a'
+		}
+
+		valid := !visited[row][col] && nextVal-startVal <= 1
+
+		return Point{row, col, grid[row][col]}, valid
+	}
+}
+
+func validMoves(grid [][]rune, start Point, visited [][]bool) []Point {
+	validMoves := make([]Point, 0)
+
+	isValidMove := isValidMoveFunc(grid, start, visited)
+
+	// up
+	if next, ok := isValidMove(start.Row-1, start.Col); ok {
+		validMoves = append(validMoves, next)
+	}
+
+	// down
+	if next, ok := isValidMove(start.Row+1, start.Col); ok {
+		validMoves = append(validMoves, next)
+	}
+
+	// left
+	if next, ok := isValidMove(start.Row, start.Col-1); ok {
+		validMoves = append(validMoves, next)
+	}
+
+	// right
+	if next, ok := isValidMove(start.Row, start.Col+1); ok {
+		validMoves = append(validMoves, next)
+	}
+
+	if debug {
+		fmt.Println(start, "Valid moves:", len(validMoves))
+	}
+
+	return validMoves
+}
+
+func bfs(grid [][]rune, start Point, end Point) int {
+	queue := make([]Point, 0)
+	visited := make([][]bool, len(grid))
+	for row := 0; row < len(grid); row += 1 {
+		visited[row] = make([]bool, len(grid[0]))
+		for col := 0; col < len(grid[0]); col += 1 {
+			visited[row][col] = false
+		}
+	}
+
+	queueConents := make(map[Point]bool)
+	queue = append(queue, start)
+	depth := 0
+	for len(queue) > 0 {
+		childNodesSize := len(queue)
+
+		if debug {
+			fmt.Println("======= Depth", depth, "========")
+		}
+
+		// process all children at this depth
+		for childNodesSize > 0 {
+			// pop first node off the queue
+			node := queue[0]
+			queue = queue[1:]
+
+			if debug {
+				fmt.Printf("%c", node.Val)
+			}
+
+			visited[node.Row][node.Col] = true
+
+			if node.Val == 'E' {
+				if debug {
+					fmt.Println()
+				}
+				return depth
+			}
+
+			validMoves := validMoves(grid, node, visited)
+			removedDups := make([]Point, 0)
+
+			for _, move := range validMoves {
+				if _, ok := queueConents[move]; !ok {
+					removedDups = append(removedDups, move)
+					queueConents[move] = true
+				}
+			}
+
+			// add the child nodes we can move to
+			queue = append(queue, removedDups...)
+
+			childNodesSize -= 1
+		}
+
+		depth += 1
+	}
+
+	return depth
+}
+
+func partOne(grid [][]rune, start Point, end Point) int {
+	return bfs(grid, start, end)
+}
+
+func main() {
+	data, err := os.ReadFile("./day12.txt")
+	checkErr(err)
+	lines := strings.Split(string(data), "\n")
+	lines = lines[:len(lines)-1] // trim empty last line
+
+	grid, start, end := buildGrid(lines)
+
+	fmt.Println("Part 1:", partOne(grid, start, end))
+}
